c6b62f59cc3688b593a4a83a43ebc95f8081a102
Paul Wilson <paulanthonywilson@gmail.com>
2015-11-04 12:03:16 +0000

    Flash the led every so often
    So we know it's switched on and working.

--- controller/controller.ino
+++ controller/controller.ino
@@ -3,14 +3,29 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  pinMode(13, OUTPUT);
   vw_setup(2000);
   
 }
 
+
 void loop() {
+  flash();
   transmit(analogRead(2), analogRead(3));
   delay(10);
 }
+
+long lastFlash = 0;
+void flash() {
+  if (millis() - lastFlash > 5000) {
+    lastFlash = millis();
+    digitalWrite(13, HIGH);
+  }
+  if (millis() - lastFlash > 50) {
+    digitalWrite(13, LOW);
+  }
+}
+
 
 void transmit(int left, int right){
   char message[50];
